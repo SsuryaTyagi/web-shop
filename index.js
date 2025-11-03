34da0cf2b0d671bcc0f4fa6341a57b1d76324edc
SsuryaTyagi <2040surya@gmail.com>
2025-11-03 16:22:59 +0530

    code update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 
 // ✅ CORS fix
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://web-shop-nine-zeta.vercel.app");
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
