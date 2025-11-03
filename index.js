72f30df1186674cd5497f2405c86a4cd1dba0985
SsuryaTyagi <2040surya@gmail.com>
2025-11-03 17:09:24 +0530

    code update

--- index.js
+++ index.js
@@ -9,13 +9,21 @@ const bestRoutes = require("./routes/bestRoutes");
 const authRouter = require("./routes/authRouter");
 
 const app = express();
+cors({
+  origin:[
+    "https://web-shop-nine-zeta.vercel.app",
+     "http://localhost:5173"
+  ]
+});
+
+
 
 app.use(express.json());
 app.use(bodyParser.json());
 
 // ✅ CORS fix
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "https://web-shop-nine-zeta.vercel.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
