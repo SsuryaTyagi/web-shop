6b3e8dd01a7b85e43f056883988e6eee3ff06440
SsuryaTyagi <2040surya@gmail.com>
2025-11-05 01:51:52 +0530

    code update

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const UserModel = require('../Models/user.js');
+const bcrypt = require("bcrypt");
+var jwt = require('jsonwebtoken');
 const authRouter = express.Router();
 
 authRouter.post('/register', async (req, res) => {
@@ -10,12 +12,15 @@ authRouter.post('/register', async (req, res) => {
     if (userExists) {
       return res.status(400).json({ message: 'User already exists' });
     }
+      // hash password 
+     const hashpassword = await bcrypt.hash(password,10)
+
 
     const registerUser = await UserModel.create({
       name,
       number,
       email,
-      password
+      password:hashpassword
     });
 
     console.log('User created:', registerUser);
@@ -28,5 +33,39 @@ authRouter.post('/register', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+authRouter.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body; 
+
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const passwordTrue = await bcrypt.compare(password, user.password);
+
+    if (!passwordTrue) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+
+    const token = await jwt.sign({ "email":user.email},process.env.JWT_TOKEN_SECRET,  { expiresIn: "2d" } );
+    res.cookie("token",token,{
+       httpOnly: true
+    })
+    return res.status(200).json({
+      message: "Login successful",
+      user,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error during login",
+      error: error.message,
+    });
+  }
+});
+
+    
+  
 
 module.exports = authRouter;
