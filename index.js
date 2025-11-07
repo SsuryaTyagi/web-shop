7a9073e6634e99d4b3575c26ccc2ce89c05e3244
SsuryaTyagi <2040surya@gmail.com>
2025-11-07 20:30:33 +0530

    code updatee

--- index.js
+++ index.js
@@ -12,11 +12,9 @@ const app = express();
 app.use(cors({
   origin: [
     "https://web-shop-frontend-l7cs.vercel.app",
-    "https://web-shop-nine-zeta.vercel.app",
-    "http://localhost:5173"
   ],
   credentials: true
-  
+
 }));
 
 
