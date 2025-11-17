b59dda19928bea4df8d8f0a2b636f2252ca7b210
SsuryaTyagi <2040surya@gmail.com>
2025-11-17 13:18:07 +0530

    major changes

--- Middlewares/auth.js
+++ Middlewares/auth.js
@@ -1,31 +1,35 @@
 const jwt = require("jsonwebtoken");
 const UserModel = require("../Models/user");
 
-const userAuth = (req, res, next)=>{
-    
-    console.log("Cookies received on server:", req.cookies);
+const userAuth = (req, res, next) => {
+  console.log("Cookies from frontend:", req.cookies);
+  console.log("Headers:", req.headers);
 
-    // verify JWT token
-    jwt.verify(req.cookies.token, process.env.JWT_TOKEN_SECRET,(err, decoded) =>{
-        if (err) {
-          return res.status(401).json({ message: 'Unauthorized access!!!' });
-        }
-        UserModel.findOne({ email: decoded.email }).then((user)=>{
-            if (!user) {
-              return res.status(404).json({ message: 'User not found' });
-            }
-            req.user = user;
-            next();
-        }).catch((err)=>{
-             res.status(500).json({
-              message: 'Error fetching user',
-              error: err
-            });
+  // verify JWT token
+  jwt.verify(req.cookies.token, process.env.JWT_TOKEN_SECRET, (err, decoded) => {
+
+      if (err) {
+        return res.status(401).json({ message: "Unauthorized access!!!" });
+      };
+      
+      UserModel.findOne({ email: decoded.email })
+        .then((user) => {
+          if (!user) {
+            return res.status(404).json({ message: "User not found" });
+          }
+          req.user = user;
+          next();
         })
-
-});
-}
+        .catch((err) => {
+          res.status(500).json({
+            message: "Error fetching user",
+            error: err,
+          });
+        });
+    }
+  );
+};
 
 module.exports = {
-    userAuth
+  userAuth,
 };
