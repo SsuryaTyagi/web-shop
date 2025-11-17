ed090e5c18c95c502a75c138ea435794b89797cb
SsuryaTyagi <2040surya@gmail.com>
2025-11-17 23:27:51 +0530

    major changes

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -75,12 +75,12 @@ authRouter.post("/login", async (req, res) => {
     console.log("Generated Token:", token);
     // Set token in cookie
 
-    //  const isProd = process.env.NODE_ENV === "production";
+    const isProd = process.env.NODE_ENV === "production";
     res.cookie("token", token, {
       httpOnly: true,
-      signed: true,
-      secure: true,
-      sameSite: "none",
+      secure: isProd, 
+      sameSite: isProd ? "none" : "lax",
+      path: "/",
     });
     return res.status(200).json({
       message: "Login successful",
