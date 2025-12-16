11c809d06df5ff94dc3345dd44bbc929645701a0
SsuryaTyagi <2040surya@gmail.com>
2025-12-16 17:07:19 +0530

    orderdetails

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -6,7 +6,7 @@ const OrderRoutes = express.Router();
 OrderRoutes.post("/create", async (req, res) => {
   const { user, items, order_total, payment_id } = req.body;
   try {
-    if ( order === ""&&user === "") {
+    if (items === ""&&user === "") {
       return res.status(500).json({
         message: "Enter full detail...",
       });
