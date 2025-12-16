c18ee10e2604aa3ef3457878064477fed32c3e4a
SsuryaTyagi <2040surya@gmail.com>
2025-12-16 17:46:23 +0530

    orderdetails

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -6,8 +6,8 @@ const OrderRoutes = express.Router();
 OrderRoutes.post("/create", async (req, res) => {
   const { user, items, order_total, payment_id } = req.body;
   try {
-    if (items === ""&&user === "") {
-      return res.status(500).json({
+    if (!user || !items || items.length === 0) {
+      return res.status(400).json({
         message: "Enter full detail...",
       });
     }
@@ -21,6 +21,7 @@ OrderRoutes.post("/create", async (req, res) => {
 
     return res.status(201).json({
       message: "Order succesfully create",
+      order,
     });
   } catch (error) {
     console.error(error);
@@ -31,9 +32,18 @@ OrderRoutes.post("/create", async (req, res) => {
 });
 OrderRoutes.post("/orderDetails", async (req, res) => {
   try {
-    const email = req.user.email;
+    const { email } = req.body;
 
-    const order = await OrderCreate.find({ email }).sort({ createdAt: -1 });
+    if (!email) {
+      return res.status(400).json({
+        success: false,
+        message: "Email required",
+      });
+    }
+
+    const order = await OrderCreate.find({
+      email: email,
+    }).sort({ createdAt: -1 });
     res.status(200).json({
       success: true,
       order,
