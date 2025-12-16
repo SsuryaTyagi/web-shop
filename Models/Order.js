75845716c0ff97579bb180254902e79c8d134749
SsuryaTyagi <2040surya@gmail.com>
2025-12-16 18:48:43 +0530

    orderdetails

--- Models/Order.js
+++ Models/Order.js
@@ -13,7 +13,7 @@ const orderSchema = new mongoose.Schema(
       {
         name: String,
         price: Number,
-        image: String,
+        img: String,
         qty: Number,
       },
     ],
